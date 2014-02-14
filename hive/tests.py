c497d16aefd823b42f9106ede6222e14ba8616bc
Atul Varma <varmaa@gmail.com>
2014-02-14 15:38:35 -0500

    split massive test suite into 3.

--- hive/tests.py
+++ hive/tests.py
@@ -5,47 +5,49 @@ from mock import patch
 from .settings_utils import set_default_env, set_default_db, \
                             parse_email_backend_url
 
-class SettingsUtilsTests(TestCase):
-    def test_set_default_env_sets_default_values(self):
+class SetDefaultEnvTests(TestCase):
+    def test_sets_default_values(self):
         with patch.dict(os.environ, {'TEST_FOO': ''}):
             del os.environ['TEST_FOO']
             set_default_env(TEST_FOO='bar')
             self.assertEqual(os.environ['TEST_FOO'], 'bar')
 
-    def test_set_default_env_does_not_overwrite_existing_values(self):
+    def test_does_not_overwrite_existing_values(self):
         with patch.dict(os.environ, {'TEST_FOO': ''}):
             set_default_env(TEST_FOO='bar')
             self.assertEqual(os.environ['TEST_FOO'], '')
 
-    def test_set_default_db_follows_env_vars(self):
+class SetDefaultDbTests(TestCase):
+    def test_follows_env_vars(self):
         with patch.dict(os.environ, {'DATABASE_URL': 'FOO', 'FOO': 'bar'}):
             set_default_db('meh')
             self.assertEqual(os.environ['DATABASE_URL'], 'bar')
 
-    def test_set_default_db_does_not_overwrite_existing_value(self):
+    def test_does_not_overwrite_existing_value(self):
         with patch.dict(os.environ, {'DATABASE_URL': 'foo'}):
             set_default_db('meh')
             self.assertEqual(os.environ['DATABASE_URL'], 'foo')
 
-    def test_set_default_db_sets_default_value(self):
+    def test_sets_default_value(self):
         with patch.dict(os.environ, {'DATABASE_URL': ''}):
             del os.environ['DATABASE_URL']
             set_default_db('meh')
             self.assertEqual(os.environ['DATABASE_URL'], 'meh')
 
-    def test_parse_email_backend_url_accepts_console(self):
+class ParseEmailBackendUrlTests(TestCase):
+    def test_accepts_console(self):
         self.assertEqual(parse_email_backend_url('console:'), {
             'EMAIL_BACKEND': 'django.core.mail.backends.console.EmailBackend'
         })
 
-    def test_parse_email_backend_url_accepts_smtp_without_auth(self):
+    def test_accepts_smtp_without_auth(self):
         self.assertEqual(parse_email_backend_url('smtp://foo.org:25'), {
             'EMAIL_BACKEND': 'django.core.mail.backends.smtp.EmailBackend',
             'EMAIL_HOST': 'foo.org',
             'EMAIL_PORT': 25
         })
 
-    def test_parse_email_backend_url_accepts_smtp_with_auth(self):
+    def test_accepts_smtp_with_auth(self):
         self.assertEqual(parse_email_backend_url('smtp://a:b@foo.org:25'), {
             'EMAIL_BACKEND': 'django.core.mail.backends.smtp.EmailBackend',
             'EMAIL_HOST': 'foo.org',
@@ -54,7 +56,7 @@ class SettingsUtilsTests(TestCase):
             'EMAIL_HOST_PASSWORD': 'b'
         })
 
-    def test_parse_email_backend_url_accepts_smtp_plus_tls(self):
+    def test_accepts_smtp_plus_tls(self):
         self.assertEqual(parse_email_backend_url('smtp+tls://foo.org:25'), {
             'EMAIL_BACKEND': 'django.core.mail.backends.smtp.EmailBackend',
             'EMAIL_USE_TLS': True,
