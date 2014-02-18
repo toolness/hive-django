4f837dedabdf1b4f8cbb2b178e3fc01caa42bc7b
Atul Varma <varmaa@gmail.com>
2014-02-18 15:49:40 -0500

    added some documentation for Organization model, and twitter name validation.

--- directory/models.py
+++ directory/models.py
@@ -1,14 +1,42 @@
+import re
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+def validate_twitter_name(value):
+    base_err = '"%s" is not a valid Twitter name.' % value
+    if len(value) > 15:
+        raise ValidationError(base_err + ' A username cannot be longer than '
+                              '15 characters.')
+    if not re.match('^[A-Za-z_]+$', value):
+        raise ValidationError(
+            base_err +
+            ' A username can only contain alphanumeric characters (letters '
+            'A-Z, numbers 0-9) with the exception of underscores.'
+        )
 
 class Organization(models.Model):
-    name = models.CharField(max_length=100)
-    website = models.URLField()
-    address = models.TextField()
-    twitter_name = models.CharField(max_length=15)
-    hive_member_since = models.DateField()
-    mission = models.TextField()
+    name = models.CharField(
+        help_text="The full name of the organization.",
+        max_length=100
+    )
+    website = models.URLField(
+        help_text="The URL of the organization's primary website."
+    )
+    address = models.TextField(
+        help_text="The full address of the organization's main office."
+    )
+    twitter_name = models.CharField(
+        max_length=15,
+        help_text="The twitter account for the organization.",
+        blank=True,
+        validators=[validate_twitter_name]
+    )
+    hive_member_since = models.DateField(
+        help_text="The date the organization joined the Hive network."
+    )
+    mission = models.TextField(
+        help_text="The organization's mission and philosophy."
+    )
 
     # TODO: How to represent youth audience?
 
--- directory/tests.py
+++ directory/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
-from .models import Organization
+from .models import Organization, validate_twitter_name
 
 class DirectoryTests(TestCase):
     def test_organization_does_not_explode(self):
         o = Organization()
+
+    def test_validate_twitter_name_rejects_invalid_names(self):
+        self.assertRaises(ValidationError, validate_twitter_name, '$')
+        self.assertRaises(ValidationError, validate_twitter_name,
+                          'reallyreallyreallyreallylongusername')
+
+    def test_validate_twitter_name_accepts_valid_names(self):
+        validate_twitter_name('toolness')
+        validate_twitter_name('t')
+        validate_twitter_name('super_burger')
