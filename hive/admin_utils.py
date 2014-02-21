482eefa5bf595d2ca0dd7eb4250fdd7661e2f210
Atul Varma <varmaa@gmail.com>
2014-02-21 15:16:44 -0500

    added a 'switch back to admin' menu option.

--- hive/admin_utils.py
+++ hive/admin_utils.py
@@ -3,17 +3,31 @@ from django.shortcuts import get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.contrib import auth
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 
-@require_POST
-@user_passes_test(lambda u: u.is_active and u.is_superuser)
-def switch_user(request, username):
-    user = get_object_or_404(User, username=username)
-
+def _switch_to(request, user):
     # http://stackoverflow.com/a/2787747
     user.backend = 'django.contrib.auth.backends.ModelBackend'
     auth.login(request, user)
 
+@require_POST
+@user_passes_test(lambda u: u.is_active and u.is_superuser)
+def switch_user(request, username):
+    superuser_name = request.user.username
+    user = get_object_or_404(User, username=username)
+
+    _switch_to(request, user)
+    request.session['user_switched_from'] = superuser_name
+
     messages.success(request, 'You are now logged in as %s.' % username)
 
+    return redirect('home')
+
+@login_required
+def switch_user_back(request):
+    superuser_name = request.session.get('user_switched_from')
+    if superuser_name:
+        user = User.objects.get(username=superuser_name, is_active=True,
+                                is_superuser=True)
+        _switch_to(request, user)
     return redirect('home')
--- hive/settings.py
+++ hive/settings.py
@@ -85,6 +85,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.media",
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
+    "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
     "hive.context_processors.origin",
     "hive.context_processors.site",
