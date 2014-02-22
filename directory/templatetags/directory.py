e8be61910728d2eabcd8170b70a352351568cf2c
Atul Varma <varmaa@gmail.com>
2014-02-21 19:01:29 -0500

    remove extraneous debugging docstring

--- directory/templatetags/directory.py
+++ directory/templatetags/directory.py
@@ -1,25 +1,3 @@
-"""
-Recursively takes a self-nested list and returns an HTML unordered list --
-WITHOUT opening and closing <ul> tags.
-
-The list is assumed to be in the proper format. For example, if ``var``
-contains: ``['States', ['Kansas', ['Lawrence', 'Topeka'], 'Illinois']]``,
-then ``{{ var|unordered_list }}`` would return::
-
-    eoaf
-    <li>States
-    <ul>
-            <li>Kansas
-            <ul>
-                    <li>Lawrence</li>
-                    <li>Topeka</li>
-            </ul>
-            </li>
-            <li>Illinois</li>
-    </ul>
-    </li>
-"""
-
 import urlparse
 from django import template
 
