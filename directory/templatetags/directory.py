69c35f35e8e876d8848820c31f2a427be7b26c41
Atul Varma <varmaa@gmail.com>
2014-04-09 11:20:06 -0400

    added emailhash template filter.

--- directory/templatetags/directory.py
+++ directory/templatetags/directory.py
@@ -1,4 +1,5 @@
 import urlparse
+import hashlib
 from django import template
 
 register = template.Library()
@@ -16,3 +17,12 @@ def get_domainname(url):
     """
 
     return urlparse.urlparse(url).hostname
+
+@register.filter(name='emailhash')
+def get_emailhash(email):
+    """
+    Return the trimmed and lowercased MD5 hash of the given
+    email address. This is useful for constructing Gravatar URLs.
+    """
+
+    return hashlib.md5(email.lower().strip()).hexdigest()
--- directory/tests/test_templatetags.py
+++ directory/tests/test_templatetags.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 
-from ..templatetags.directory import get_domainname
+from ..templatetags.directory import get_domainname, get_emailhash
 
 class TemplateTagsAndFiltersTests(TestCase):
     def test_get_domainname(self):
         self.assertEqual(get_domainname('http://foo.org:34'), 'foo.org')
+
+    def test_get_emailhash(self):
+        # http://en.gravatar.com/site/implement/hash/
+        self.assertEqual(get_emailhash(' MyEmailAddress@example.com'),
+                         '0bc83cb571cd1c50ba6f3e8a78ef1346')
