5d12e60afde09b5ae6f1795ca3afc4eda018ab7f
Atul Varma <varmaa@gmail.com>
2014-04-11 10:06:19 -0400

    enhanced the emailimportedusers command.

--- directory/management/commands/emailimportedusers.py
+++ directory/management/commands/emailimportedusers.py
@@ -1,4 +1,5 @@
 import urlparse
+from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.forms import PasswordResetForm
 from django.conf import settings
@@ -6,13 +7,50 @@ from django.conf import settings
 from directory.models import ImportedUserInfo
 
 class Command(BaseCommand):
-    help = 'Email imported users about their new account.'
+    option_list = BaseCommand.option_list + (
+        make_option('--dry-run',
+            dest='dry_run',
+            default=False,
+            help='don\'t actually send emails',
+            action='store_true'
+        ),
+        make_option('--force',
+            dest='force',
+            default=False,
+            help='send emails even to those who were already emailed',
+            action='store_true'
+        )
+    )
+
+    help = '''\
+    Email imported users about their new account, excluding 
+    users who have already been emailed.
+
+    If no arguments are passed, everyone who hasn't yet been emailed will
+    be sent an email. Otherwise, only the users with the given email
+    addresses will be contacted.
+    '''
+
+    args = '[email1] [email2] ...'
+
+    def send_email(self, email):
+        form = PasswordResetForm({'email': email})
+        if not form.is_valid(): raise AssertionError('Form is not valid')
+        form.save(
+            use_https=urlparse.urlparse(settings.ORIGIN).scheme == 'https',
+            subject_template_name='directory/importeduser_subject.txt',
+            email_template_name='directory/importeduser_email.html'
+        )
 
     def handle(self, *args, **kwargs):
-        use_https = (urlparse.urlparse(settings.ORIGIN).scheme == 'https')
-        non_emailed_userinfo = ImportedUserInfo.objects.filter(
-            was_sent_email=False
-        )
+        criteria = {}
+        if not kwargs['force']: criteria['was_sent_email'] = False
+        non_emailed_userinfo = ImportedUserInfo.objects.filter(**criteria)
+        if args:
+            non_emailed_userinfo = [
+                info for info in non_emailed_userinfo
+                if info.user.email in args
+            ]
         write = self.stdout.write
         if len(non_emailed_userinfo) == 0:
             write('No users need to be emailed.')
@@ -20,12 +58,7 @@ class Command(BaseCommand):
         for info in non_emailed_userinfo:
             user = info.user
             write("  Emailing %s <%s>" % (user.get_full_name(), user.email))
-            form = PasswordResetForm({'email': user.email})
-            if not form.is_valid(): raise AssertionError('Form is not valid')
-            form.save(
-                use_https=use_https,
-                subject_template_name='directory/importeduser_subject.txt',
-                email_template_name='directory/importeduser_email.html'
-            )
-            info.was_sent_email = True
-            info.save()
+            if not kwargs['dry_run']:
+                self.send_email(user.email)
+                info.was_sent_email = True
+                info.save()
