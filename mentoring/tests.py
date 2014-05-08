97fe922b419a23873a8c5c81e06255e419be1b27
Atul Varma <varmaa@gmail.com>
2014-05-08 10:38:06 -0400

    added basic tests for mentoring app.

--- mentoring/tests.py
+++ mentoring/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from directory.models import Expertise
+from directory.tests.test_views import WnycTestCase
 
-# Create your tests here.
+class MentoringTests(WnycTestCase):
+    def setUp(self):
+        WnycTestCase.setUp(self)
+        Expertise(category='other',
+                  details='I am awesome',
+                  user=self.wnyc_member).save()
+
+    def test_index_lists_mentor_counts(self):
+        self.login_as_wnyc_member()
+        response = self.client.get('/mentoring/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '1 mentor')
+        self.assertContains(response, '0 mentors')
+
+    def test_category_page_lists_mentors(self):
+        self.login_as_wnyc_member()
+        response = self.client.get('/mentoring/other/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Brian Lehrer')
