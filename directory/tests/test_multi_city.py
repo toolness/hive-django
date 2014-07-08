904c9b8abc66df32a195d4cd87e1ead8aae08cc1
Atul Varma <varmaa@gmail.com>
2014-07-08 08:14:16 -0400

    added tests for the city_scoped decorator.

--- directory/tests/test_multi_city.py
+++ directory/tests/test_multi_city.py
@@ -1,14 +1,47 @@
 from django.test import TestCase
 from django.contrib.sites.models import Site
+from django.test.utils import override_settings
 
 from ..multi_city import is_multi_city
 
-class MultiCityTests(TestCase):
-    def test_is_multi_city_returns_true_when_city_does_not_exist(self):
-        site = Site(name='Foo', domain='foo.com')
-        self.assertTrue(is_multi_city(site=site))
+def using_multi_city_site(f):
+    def wrapper(self):
+        site = Site(domain='globalhivedir.com', name='Global Hive Dir')
+        site.save()
+        with override_settings(SITE_ID=site.id):
+            return f(self)
+    return wrapper
 
-    def test_is_multi_city_returns_false_when_city_exists(self):
-        # By default, we have a single default city associated w/ the 
-        # single default site, so is_multi_city() will use that.
+class IsMultiCityTests(TestCase):
+    @using_multi_city_site
+    def test_returns_true_when_city_does_not_exist(self):
+        self.assertTrue(is_multi_city())
+
+    def test_returns_false_when_city_exists(self):
         self.assertFalse(is_multi_city())
+
+class CityScopedTests(TestCase):
+    fixtures = ['chicago.json']
+
+    def test_single_city_sites_ignore_multi_city_urls(self):
+        response = self.client.get('/chicago/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_single_city_sites_accept_single_city_urls(self):
+        response = self.client.get('/activity/')
+        self.assertEqual(response.status_code, 302)
+
+    @using_multi_city_site
+    def test_multi_city_sites_accept_multi_city_urls(self):
+        response = self.client.get('/chicago/')
+        self.assertEqual(response.status_code, 200)
+
+    @using_multi_city_site
+    def test_multi_city_sites_404_on_invalid_city_slugs(self):
+        response = self.client.get('/podunk/')
+        self.assertEqual(response.status_code, 404)
+
+    @using_multi_city_site
+    def test_multi_city_sites_ignore_single_city_urls(self):
+        response = self.client.get('/activity/')
+        self.assertEqual(response.status_code, 404)
