96098ac8153d4481888b6820015aedb1f4467899
Atul Varma <varmaa@gmail.com>
2014-07-11 07:59:55 -0400

    don't let city editors change is_staff, it's useless by itself.

--- directory/admin.py
+++ directory/admin.py
@@ -146,7 +146,7 @@ class MembershipUserAdmin(UserAdmin):
     editor_fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff')}),
+        (_('Permissions'), {'fields': ('is_active',)}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
 
