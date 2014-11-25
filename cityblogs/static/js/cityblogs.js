954c6c136a50863f6cdc177b69b95c2aa9b0fed6
Atul Varma <varmaa@gmail.com>
2014-11-25 18:14:15 -0500

    fade-in related blog posts panel.

--- cityblogs/static/js/cityblogs.js
+++ cityblogs/static/js/cityblogs.js
@@ -2,8 +2,10 @@ $(function() {
   $('[data-cityblogs-splat-url]').each(function() {
     var $this = $(this);
     var url = $this.attr('data-cityblogs-splat-url');
+    $this.hide();
     $.get(url, function(html, textStatus, jqXHR) {
       $this.html(html);
+      $this.fadeIn();
     });
   });
 });
